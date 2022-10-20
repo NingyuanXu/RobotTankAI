cb30f8602cce3d39606d6fbc1b78d8da183ddd56
Ningyuan Xu <xu.ningyuan@outlook.com>
2022-10-19 21:47:09 -0700

    change a bit the states implementation:

--- src/main/RobotStates.java
+++ src/main/RobotStates.java
@@ -5,43 +5,72 @@ public class RobotStates {
     public static int maxDistance = 1000; // 1000 is the maximum distance in the game
     public static double initialEnergy; // initial energy in the game
 
-    public static final int numDirections = 4; // head, right, back, left
-    public static final int numEnemyBearing = 4; // same
+    public static final int numDirections = 4;
+    public static final int numEnemyBearing = 4;
     public static final int numEnergy = 4; // low, less than half , more than half, high
     public static final int hitWall = 2; // hit or wall
     public static final int hitByBullet = 2; // be shot or not
     public static final int numEnemyDistance = 10; // max is 1000
 
-    public static final int numStates = numDirections * numEnemyBearing * numEnergy * hitByBullet * hitWall * numEnemyDistance;
+    public static int numStates = 0;
+    public static int stateMap[][][][][][] = new int[numEnemyDistance][numDirections][numEnemyBearing][numEnergy][hitWall][hitByBullet];
+
+    static {
+        for (int a = 0; a < numEnemyDistance; a++)
+            for (int b = 0; b < numDirections; b++)
+                        for (int c = 0; c < numEnemyBearing; c++)
+                            for (int d = 0; d < numEnergy; d++)
+                                for (int e = 0; e < hitWall; e++)
+                                    for (int f = 0; f < hitByBullet; f++)
+                                        stateMap[a][b][c][d][e][f] = numStates++;
+    }
 
     public static int getStateIndex(int direction, int bearing, int energy, int bullet, int wall, int distance) {
-        int index = distance + wall * numEnemyDistance + bullet * numEnemyDistance * hitWall +
-                    energy * numEnemyDistance * hitWall * hitByBullet + bearing * numEnemyDistance * hitWall * hitByBullet * numEnergy +
-                    direction * numEnemyDistance * hitWall * hitByBullet * numEnergy * numEnemyBearing;
-        assert(index < numStates);
-        return index;
+        return stateMap[distance][direction][bearing][energy][wall][bullet];
     }
 
-    // Divide 360 degree into four directions, up, down, right, left
+    // Input is in degree 0 to 360 degrees
     public static int getDirectionState(double eventHeading) {
-        double heading = eventHeading + 45.0;
-        return (int)(heading > 360.0 ? ((heading - 360.0) / 90.0) : (heading / 90.0));
+        if ((eventHeading >= 0.0 && eventHeading < 45.0) || (eventHeading <= 360.0 && eventHeading > 315.0)) {
+            return 0;
+        } else if (eventHeading >= 45.0 && eventHeading < 135.0) {
+            return 1;
+        } else if (eventHeading >= 135.0 && eventHeading < 225.0) {
+            return 3;
+        } else {
+            return 2;
+        }
     }
 
-    // Bearings can be negative degrees
     public static int getEnemyBearingState(double eventEnemyBearing) {
         eventEnemyBearing = eventEnemyBearing < 0.0 ? eventEnemyBearing + 360.0 : eventEnemyBearing;
         double bearing = eventEnemyBearing + 45.0;
-        return (int)(bearing > 360.0 ? ((bearing - 360.0) / 90.0) : (bearing / 90.0));
+        if ((bearing >= 0.0 && bearing < 45.0) || (bearing <= 360.0 && bearing > 315.0)) {
+            return 0;
+        } else if (bearing >= 45.0 && bearing < 135.0) {
+            return 1;
+        } else if (bearing >= 135.0 && bearing < 225.0) {
+            return 3;
+        } else {
+            return 2;
+        }
     }
 
     public static int getDistanceState(double eventDistance) {
-        int distance = (int)eventDistance / (maxDistance / numEnemyDistance);
-        return distance == numEnemyDistance ? numEnemyDistance - 1 : distance;
+        int distanceRanges = maxDistance / numEnemyDistance;
+        int currentDistance = (int) eventDistance / distanceRanges;
+        return (currentDistance < numEnemyDistance) ? currentDistance: numEnemyDistance - 1;
     }
 
     public static int getEnergyState(double eventRobotEnergy) {
-        int energy = (int)(eventRobotEnergy / (initialEnergy / (double)numEnergy));
-        return energy == numEnergy ? numEnergy - 1 : energy;
+        if (eventRobotEnergy < 25.0) {
+            return 0;
+        } else if (eventRobotEnergy < 50.0) {
+            return 1;
+        } else if (eventRobotEnergy < 75.0) {
+            return 2;
+        } else {
+            return 3;
+        }
     }
 }
